90494da516096833a66f655828c5f55d62c0c3ec
Louell Sala <louell_sala@yahoo.com>
2022-02-18 23:28:19 -0500

    added images in eshop and fixed the my collection page

--- frontend/pages/market.js
+++ frontend/pages/market.js
@@ -2,21 +2,143 @@ import Image from "next/image";
 
 const Market = () => {
     return ( 
-        <div className="w-full max-w-md bg-white border rounded shadow-lg md:w-1/2 border-palette-lighter">
-        
-        <h1 className="py-2 mt-4 text-4xl font-extrabold leading-relaxed text-center font-primary text-palette-primary sm:py-4">
-                SHOP 
-             </h1>
-        
-             <Image
-                src="/bag.jpeg"
-                alt="bag"
-                width={500}
-                height={300}
-                />
-        
+        <div className="">
+   
+<div className="flex items-center w-screen min-h-screen bg-indigo-100">
+  <div className="container flex flex-wrap items-start ml-auto mr-auto">
+   
+    <div className="w-full pl-5 mt-4 mb-4 lg:pl-2">
+    
+    <h1 className="text-3xl font-extrabold text-gray-700 lg:text-4xl">
+        SHOP NOW
+    </h1>
+    <p>Up to 70% OFF</p>
+</div>
+    
+
+    <div className="w-full pl-5 pr-5 mb-5 md:w-1/2 lg:w-1/4 lg:pl-2 lg:pr-2">
+      <div className="p-2 transition duration-300 transform bg-white rounded-lg m-h-64 hover:translate-y-2 hover:shadow-xl">
+        <figure className="mb-2">
+          <img src="/bag.jpeg" alt="" class="h-50 ml-auto mr-auto" />
+        </figure>
+        <div className="flex flex-col p-4 bg-purple-700 rounded-lg">
+          <div>
+            <h5 className="text-2xl font-bold leading-none text-white">
+              HANDBAG
+            </h5>
+            <span className="text-xs leading-none text-gray-400">GUCCI</span>
+          </div>
+          <div className="flex items-center">
+            <div className="text-lg font-light text-white">
+              $1099,00
+            </div>
+            <button href="javascript:;" class="rounded-full bg-purple-700 text-white hover:bg-white hover:text-purple-900 hover:shadow-xl focus:outline-none w-10 h-10 flex ml-auto transition duration-300">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="stroke-current m-auto">
+                <line x1="12" y1="5" x2="12" y2="19"></line>
+                <line x1="5" y1="12" x2="19" y2="12"></line>
+              </svg>
+            </button>
+          </div>
         </div>
-    )        
+      </div>
+    </div>
+
+      
+    <div className="w-full pl-5 pr-5 mb-5 md:w-1/2 lg:w-1/4 lg:pl-2 lg:pr-2">
+      <div className="p-2 transition duration-300 transform bg-white rounded-lg m-h-64 hover:translate-y-2 hover:shadow-xl">
+        <figure className="mb-2">
+          <img src="/bag03.jpeg" alt="" class="h-50 ml-auto mr-auto" />
+        </figure>
+        <div className="flex flex-col p-4 bg-purple-700 rounded-lg">
+          <div>
+            <h5 className="text-2xl font-bold leading-none text-white">
+              HANDBAG
+            </h5>
+            <span className="text-xs leading-none text-gray-400">GUCCI</span>
+          </div>
+          <div className="flex items-center">
+            <div className="text-lg font-light text-white">
+              $1099,00
+            </div>
+            <button href="javascript:;" class="rounded-full bg-purple-700 text-white hover:bg-white hover:text-purple-900 hover:shadow-xl focus:outline-none w-10 h-10 flex ml-auto transition duration-300">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="stroke-current m-auto">
+                <line x1="12" y1="5" x2="12" y2="19"></line>
+                <line x1="5" y1="12" x2="19" y2="12"></line>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+   
+    <div className="w-full pl-5 pr-5 mb-5 md:w-1/2 lg:w-1/4 lg:pl-2 lg:pr-2">
+      <div className="p-2 transition duration-300 transform bg-white rounded-lg m-h-64 hover:translate-y-2 hover:shadow-xl">
+        <figure className="mb-2">
+          <img src="/bag05.jpeg" alt="" class="h-50 ml-auto mr-auto" />
+        </figure>
+        <div className="flex flex-col p-4 bg-purple-700 rounded-lg">
+          <div>
+            <h5 className="text-2xl font-bold leading-none text-white">
+              HANDBAG
+            </h5>
+            <span className="text-xs leading-none text-gray-400">GUCCI</span>
+          </div>
+          <div className="flex items-center">
+            <div className="text-lg font-light text-white">
+              $8000
+            </div>
+            <button href="javascript:;" class="rounded-full bg-purple-700 text-white hover:bg-white hover:text-purple-900 hover:shadow-xl focus:outline-none w-10 h-10 flex ml-auto transition duration-300">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="stroke-current m-auto">
+                <line x1="12" y1="5" x2="12" y2="19"></line>
+                <line x1="5" y1="12" x2="19" y2="12"></line>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+      
+    <div className="w-full pl-5 pr-5 mb-5 md:w-1/2 lg:w-1/4 lg:pl-2 lg:pr-2">
+      <div className="p-2 transition duration-300 transform bg-white rounded-lg m-h-64 hover:translate-y-2 hover:shadow-xl">
+        <figure className="mb-2">
+          <img src="/bag04.jpeg" alt="" class="h-50 ml-auto mr-auto" />
+        </figure>
+        <div className="flex flex-col p-4 bg-purple-700 rounded-lg">
+          <div>
+            <h5 className="text-2xl font-bold leading-none text-white">
+              HANDBAG
+            </h5>
+            <span className="text-xs leading-none text-gray-400">GUCCI</span>
+          </div>
+          <div className="flex items-center">
+            <div className="text-lg font-light text-white">
+              $2999,00
+            </div>
+            <button href="javascript:;" class="rounded-full bg-purple-700 text-white hover:bg-white hover:text-purple-900 hover:shadow-xl focus:outline-none w-10 h-10 flex ml-auto transition duration-300">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="stroke-current m-auto">
+                <line x1="12" y1="5" x2="12" y2="19"></line>
+                <line x1="5" y1="12" x2="19" y2="12"></line>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+</div>
+
+
+
+
+
+      
+</div>
+)        
 }
  
 export default Market;
