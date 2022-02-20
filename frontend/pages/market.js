36537b9626a4c34df3a2a9c34d459c8eb42bcd67
Louell Sala <louell_sala@yahoo.com>
2022-02-19 20:17:36 -0500

    fixed the card errors

--- frontend/pages/market.js
+++ frontend/pages/market.js
@@ -19,7 +19,7 @@ const Market = () => {
     <div className="w-full pl-5 pr-5 mb-5 md:w-1/2 lg:w-1/4 lg:pl-2 lg:pr-2">
       <div className="p-2 transition duration-300 transform bg-white rounded-lg m-h-64 hover:translate-y-2 hover:shadow-xl">
         <figure className="mb-2">
-          <img src="/bag.jpeg" alt="" class="h-50 ml-auto mr-auto" />
+          <img src="/bag.jpeg" alt="/" className="ml-auto mr-auto h-50" />
         </figure>
         <div className="flex flex-col p-4 bg-purple-700 rounded-lg">
           <div>
@@ -32,8 +32,8 @@ const Market = () => {
             <div className="text-lg font-light text-white">
               $1099,00
             </div>
-            <button href="javascript:;" class="rounded-full bg-purple-700 text-white hover:bg-white hover:text-purple-900 hover:shadow-xl focus:outline-none w-10 h-10 flex ml-auto transition duration-300">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="stroke-current m-auto">
+            <button href="#" className="flex w-10 h-10 ml-auto text-white transition duration-300 bg-purple-700 rounded-full hover:bg-white hover:text-purple-900 hover:shadow-xl focus:outline-none">
+              <svg xmlns="/" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="m-auto stroke-current">
                 <line x1="12" y1="5" x2="12" y2="19"></line>
                 <line x1="5" y1="12" x2="19" y2="12"></line>
               </svg>
@@ -47,7 +47,7 @@ const Market = () => {
     <div className="w-full pl-5 pr-5 mb-5 md:w-1/2 lg:w-1/4 lg:pl-2 lg:pr-2">
       <div className="p-2 transition duration-300 transform bg-white rounded-lg m-h-64 hover:translate-y-2 hover:shadow-xl">
         <figure className="mb-2">
-          <img src="/bag03.jpeg" alt="" class="h-50 ml-auto mr-auto" />
+          <img src="/bag03.jpeg" alt="" className="ml-auto mr-auto h-50" />
         </figure>
         <div className="flex flex-col p-4 bg-purple-700 rounded-lg">
           <div>
@@ -60,8 +60,8 @@ const Market = () => {
             <div className="text-lg font-light text-white">
               $1099,00
             </div>
-            <button href="javascript:;" class="rounded-full bg-purple-700 text-white hover:bg-white hover:text-purple-900 hover:shadow-xl focus:outline-none w-10 h-10 flex ml-auto transition duration-300">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="stroke-current m-auto">
+            <button href="#" className="flex w-10 h-10 ml-auto text-white transition duration-300 bg-purple-700 rounded-full hover:bg-white hover:text-purple-900 hover:shadow-xl focus:outline-none">
+              <svg xmlns="/" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="m-auto stroke-current">
                 <line x1="12" y1="5" x2="12" y2="19"></line>
                 <line x1="5" y1="12" x2="19" y2="12"></line>
               </svg>
@@ -76,7 +76,7 @@ const Market = () => {
     <div className="w-full pl-5 pr-5 mb-5 md:w-1/2 lg:w-1/4 lg:pl-2 lg:pr-2">
       <div className="p-2 transition duration-300 transform bg-white rounded-lg m-h-64 hover:translate-y-2 hover:shadow-xl">
         <figure className="mb-2">
-          <img src="/bag05.jpeg" alt="" class="h-50 ml-auto mr-auto" />
+          <img src="/bag05.jpeg" alt="" className="ml-auto mr-auto h-50" />
         </figure>
         <div className="flex flex-col p-4 bg-purple-700 rounded-lg">
           <div>
@@ -89,8 +89,8 @@ const Market = () => {
             <div className="text-lg font-light text-white">
               $8000
             </div>
-            <button href="javascript:;" class="rounded-full bg-purple-700 text-white hover:bg-white hover:text-purple-900 hover:shadow-xl focus:outline-none w-10 h-10 flex ml-auto transition duration-300">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="stroke-current m-auto">
+            <button href="#" className="flex w-10 h-10 ml-auto text-white transition duration-300 bg-purple-700 rounded-full hover:bg-white hover:text-purple-900 hover:shadow-xl focus:outline-none">
+              <svg xmlns="/" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="m-auto stroke-current">
                 <line x1="12" y1="5" x2="12" y2="19"></line>
                 <line x1="5" y1="12" x2="19" y2="12"></line>
               </svg>
@@ -104,7 +104,7 @@ const Market = () => {
     <div className="w-full pl-5 pr-5 mb-5 md:w-1/2 lg:w-1/4 lg:pl-2 lg:pr-2">
       <div className="p-2 transition duration-300 transform bg-white rounded-lg m-h-64 hover:translate-y-2 hover:shadow-xl">
         <figure className="mb-2">
-          <img src="/bag04.jpeg" alt="" class="h-50 ml-auto mr-auto" />
+          <img src="/bag04.jpeg" alt="" className="ml-auto mr-auto h-50" />
         </figure>
         <div className="flex flex-col p-4 bg-purple-700 rounded-lg">
           <div>
@@ -117,8 +117,8 @@ const Market = () => {
             <div className="text-lg font-light text-white">
               $2999,00
             </div>
-            <button href="javascript:;" class="rounded-full bg-purple-700 text-white hover:bg-white hover:text-purple-900 hover:shadow-xl focus:outline-none w-10 h-10 flex ml-auto transition duration-300">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="stroke-current m-auto">
+            <button href="#" className="flex w-10 h-10 ml-auto text-white transition duration-300 bg-purple-700 rounded-full hover:bg-white hover:text-purple-900 hover:shadow-xl focus:outline-none">
+              <svg xmlns="/" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="m-auto stroke-current">
                 <line x1="12" y1="5" x2="12" y2="19"></line>
                 <line x1="5" y1="12" x2="19" y2="12"></line>
               </svg>
