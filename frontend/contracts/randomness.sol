a763faf2e1d339f2fe58d2c3017e0691f1874d1f
mrhouzlane <mrhouzlane@github.com>
2022-03-19 23:08:21 +0100

    changes vrf-1

--- frontend/contracts/randomness.sol
+++ frontend/contracts/randomness.sol
@@ -1,82 +1,71 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >= 0.8.0;
+pragma solidity >=0.8.0;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
-/// @title A guessing game 
-/// @author Mehdi R. 
+/// @title VRF
+/// @author Mehdi R.
 /// @notice You can use this contract for only the most basic simulation
 /// @dev All function calls are currently implemented without side effects
-/// @custom:experimental This is only for testing use. 
+/// @custom:experimental This is only for testing use.
 contract Randomness is VRFConsumerBase {
-
-    uint public //;
-    //uint public minimumBet; 
-    //uint private winner; 
-    //uint [] guesses; 
-
-
     bytes32 internal keyHash;
     uint256 internal fee;
-    
+
     uint256 public randomResult;
 
-    // List of the 5 users who made a bet 
-    address payable [] public users ; 
-
+    // List of the 5 users who made a bet
+    address payable[] public users;
 
     struct user {
-        uint amount;
-        uint article; 
+        uint256 amount;
+        uint256 article;
     }
-  
-
-    event userWithdrawal(address user, uint amount);
-
 
     //// CONSTRUCTOR
 
-     /**
+    /**
      * Constructor inherits VRFConsumerBase
-     * 
+     *
      * Network: Mumbai Testnet
      * Chainlink VRF Coordinator address: 0x8C7382F9D8f56b33781fE506E897a4F1e2d17255
      * LINK token address:                0x326C977E6efc84E512bB9C30f76E30c160eD06FB
      * Key Hash: 0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4
      */
 
-     //https://docs.chain.link/docs/vrf-contracts/v1/ for addresses
-    constructor() 
+    //https://docs.chain.link/docs/vrf-contracts/v1/ for addresses
+    constructor()
         VRFConsumerBase(
             0x8C7382F9D8f56b33781fE506E897a4F1e2d17255, // VRF Coordinator
-            0x326C977E6efc84E512bB9C30f76E30c160eD06FB  // LINK Token
+            0x326C977E6efc84E512bB9C30f76E30c160eD06FB // LINK Token
         )
     {
         keyHash = 0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4;
-        fee = 0.1 * 10 ** 18 ; // 0.1 LINK (Varies by network)
-        //minimumDeposit = 100; //0.0011 ether 
-        //maxusers = 5; 
+        fee = 0.1 * 10**18; // 0.1 LINK (Varies by network)
+        //minimumDeposit = 100; //0.0011 ether
+        //maxusers = 5;
     }
 
-
-    /** 
-     * Requests randomness 
+    /**
+     * Requests randomness
      */
     function getRandomNumber() public returns (bytes32 requestId) {
-        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
+        require(
+            LINK.balanceOf(address(this)) >= fee,
+            "Not enough LINK - fill contract with faucet"
+        );
         return requestRandomness(keyHash, fee);
     }
 
     /**
      * Callback function used by VRF Coordinator
      */
-    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+    function fulfillRandomness(bytes32 requestId, uint256 randomness)
+        internal
+        override
+    {
         // get random number between 1 and 1000
-        randomResult = randomness % 1000 + 1;
+        randomResult = (randomness % 1000) + 1;
     }
-
-
-    
-
 }
